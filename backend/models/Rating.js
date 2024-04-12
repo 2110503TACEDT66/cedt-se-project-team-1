5475e08cd53f2f74849a5935a03656590796be97
Peem Supanat <83868836+PeemSx@users.noreply.github.com>
2024-04-12 17:54:58 +0700

    add more attributes to rating shemas

--- backend/models/Rating.js
+++ backend/models/Rating.js
@@ -34,6 +34,18 @@ const ratingSchema = new mongoose.Schema({
         required: true,
         default: "No comment"
     },
+    overallRating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5,
+        default: 0
+    },
+    shopOwner:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'ShopOwner',
+        required: true
+    },
     user: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
