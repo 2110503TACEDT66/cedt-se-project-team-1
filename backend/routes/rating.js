f12a786376793a925ad603b178bf3a6c1a07d699
Tanongsak Anurak <tanongsak5913@gmail.com>
2024-04-13 15:21:25 +0700

    remove getAvgRating

--- backend/routes/rating.js
+++ backend/routes/rating.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const {getRating, getRatings, updateRating, deleteRating,addRating, getAvgRatings, getAvgRating } 
-= require('../controllers/rating.js');
+const { getRating, getRatings, updateRating, deleteRating, addRating } = require('../controllers/rating.js');
 
 const router = express.Router({ mergeParams: true });
 
 const { protect, authorize } = require('../middleware/auth');
 
-router.route('/avg')
-    .get(getAvgRatings);
-router.route('/avg/:id')
-    .get(getAvgRating);
-    
 router.route('/:id')
     .get(getRating)
     .put(protect, authorize('admin', 'user'), updateRating)
@@ -21,5 +15,5 @@ router.route('/')
     .post(protect, authorize('admin', 'user'), addRating);
 
 
-    
+
 module.exports = router;
