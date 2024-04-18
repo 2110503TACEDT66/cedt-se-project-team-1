47e9dc46a99d2bcdc400df3dc8043b2e7a17cd00
Tanongsak Anurak <tanongsak5913@gmail.com>
2024-04-18 21:32:09 +0700

    massage catalog responsive fix

--- frontend/src/components/MassageCatalog.tsx
+++ frontend/src/components/MassageCatalog.tsx
@@ -3,10 +3,15 @@ import Link from "next/link";
 import Card from "./Card";
 import { MassageItem } from "../../interface";
 
-export default function MassageCatalog({ massages }: { massages: MassageItem[] }) {
-    const [sortOrder, setSortOrder] = useState<"lowToHigh" | "highToLow" | "AtoZ" | "ZtoA" >("highToLow");
+export default function MassageCatalog({
+    massages,
+}: {
+    massages: MassageItem[];
+}) {
+    const [sortOrder, setSortOrder] = useState<
+        "lowToHigh" | "highToLow" | "AtoZ" | "ZtoA"
+    >("highToLow");
     const [searchTerm, setSearchTerm] = useState("");
-
 
     const filteredMassages = massages.filter((massage) =>
         massage.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -24,8 +29,8 @@ export default function MassageCatalog({ massages }: { massages: MassageItem[] }
     });
 
     return (
-        <>                
-            <div className="flex flex-row justify-center gap-4 my-10">                
+        <>
+            <div className="flex flex-row justify-center gap-4 my-10">
                 <input
                     type="text"
                     placeholder="Search..."
@@ -36,7 +41,15 @@ export default function MassageCatalog({ massages }: { massages: MassageItem[] }
                 <select
                     title="Sort Order"
                     value={sortOrder}
-                    onChange={(e) => setSortOrder(e.target.value as "lowToHigh" | "highToLow" | "AtoZ" | "ZtoA")}
+                    onChange={(e) =>
+                        setSortOrder(
+                            e.target.value as
+                                | "lowToHigh"
+                                | "highToLow"
+                                | "AtoZ"
+                                | "ZtoA"
+                        )
+                    }
                     className="border border-gray-300 rounded-md px-2 py-1"
                 >
                     <option value="lowToHigh">Rating Low to High</option>
@@ -44,16 +57,22 @@ export default function MassageCatalog({ massages }: { massages: MassageItem[] }
                     <option value="AtoZ">Name A to Z</option>
                     <option value="ZtoA">Name: Z to A</option>
                 </select>
-
             </div>
             <div className="flex flex-row content-around flex-wrap gap-20 justify-center mt-8">
-
                 {sortedMassages.map((massage: MassageItem) => (
-                    <Link href={`/massage/${massage.id}`} key={massage.id} className="w-1/5">
+                    <Link
+                        href={`/massage/${massage.id}`}
+                        key={massage.id}
+                        className=""
+                    >
                         <Card
                             key={massage.id}
                             massageName={massage.name}
-                            imgSrc={massage.picture === "no-photo" ? "/img/massage-default.jpg" : massage.picture}
+                            imgSrc={
+                                massage.picture === "no-photo"
+                                    ? "/img/massage-default.jpg"
+                                    : massage.picture
+                            }
                             massageId={massage.id}
                             masssageDescription={massage.description}
                             massageDistricts={massage.district}
