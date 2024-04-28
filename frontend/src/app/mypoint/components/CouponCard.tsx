142e39af5f34632df5fbdfe9620924c8fdc25d01
Pongsaky <pongsakon.kaew@gmail.com>
2024-04-29 01:17:18 +0700

    Add edit and delete button

--- frontend/src/app/mypoint/components/CouponCard.tsx
+++ frontend/src/app/mypoint/components/CouponCard.tsx
@@ -13,16 +13,19 @@ import addCustomerCoupon from '@/libs/CustomerCoupon/addCustomerCoupon'
 import updateUserPoint from '@/libs/User/updateUserPoint'
 import { setPoint } from '@/redux/features/userSlice'
 
-export default function CouponCard({couponItems,userPoint,updateUserPoint,session}:
-    {couponItems:CouponItem
-    userPoint:number
-    updateUserPoint:(newPoint:number)=>void
-    session:any
+import ModalButton from '@/components/ModalButton'
+
+export default function CouponCard({ couponItems, userPoint, updateUserPoint, session }:
+    {
+        couponItems: CouponItem
+        userPoint: number
+        updateUserPoint: (newPoint: number) => void
+        session: any
     }) {
     const dispatch = useDispatch<AppDispatch>()
     const [massageShop, setMassageShop] = React.useState<MassageItem>({} as MassageItem);
     const [canBuy, setCanBuy] = React.useState<boolean>(true);
-    
+
     useEffect(() => {
         getMassage(couponItems.massageShop).then((res) => {
             const massage: MassageItem = res.data;
@@ -31,38 +34,49 @@ export default function CouponCard({couponItems,userPoint,updateUserPoint,sessio
     }, []);
 
     const handleBuy = async () => {
-        if(userPoint > couponItems.point){
+        if (userPoint > couponItems.point) {
             await addCustomerCoupon(couponItems._id, session?.user.data._id ?? '', massageShop._id).then((res) => {
-                if(res.success){
+                if (res.success) {
                     setCanBuy(false);
                     updateUserPoint(userPoint - couponItems.point);
                 }
             });
-        }else alert('You do not have enough points to buy this coupon')
-        
+        } else alert('You do not have enough points to buy this coupon')
+
     }
-return (
-    <div className='w-[350px] h-[180px] bg-white shadow-md'>
+    return (
+        <div className='w-[350px] h-[180px] bg-white shadow-md'>
             <div className='flex flex-row'>
-                    <div className='bg-[#426B1F] h-[180px] w-[120px] flex justify-center items-center'>
-                            <Image src='/img/discount.png' width={60} height={60} alt='coupon'/>
-                    </div>
-                    <div className='w-full h-[180px] p-4'>
+                <div className='bg-[#426B1F] h-[180px] w-[120px] flex justify-center items-center'>
+                    <Image src='/img/discount.png' width={60} height={60} alt='coupon' />
+                </div>
+                <div className='w-full h-[180px] p-4'>
+                    <div className='flex'>
+                        <div>
                             <Typography variant='h5' fontWeight={"bold"}>{couponItems.discount}%</Typography>
                             <Typography variant='body1' fontWeight={"bold"}>Max {couponItems.coverage} Bath</Typography>
                             <Typography variant='body2'>{couponItems.point} point</Typography>
                             <Typography variant='body2'>{massageShop.name}</Typography>
-                            <div className='flex justify-end items-end mt-5'>
-                                { canBuy ?
-                                    <RedeemButton onClick={handleBuy}>Buy this Coupon</RedeemButton> :
-                                    <Button variant='contained' disabled>Bought</Button> 
-                                }
-                                
-                            </div>  
+                        </div>
+                        <div className='flex flex-col ml-4 gap-1 mt-[-10px]'>
+                            <ModalButton text='Edit' color='gray'>
+                                <></>
+                            </ModalButton>
+                            <div>
+                                <button className="rounded-md bg-red-600 hover:bg-red-800 transition px-3 py-1 text-white shadow-sm relative">Delete</button>
+                            </div>
+                        </div>
                     </div>
+                    <div className='flex justify-end items-end mt-5'>
+                        {canBuy ?
+                            <RedeemButton onClick={handleBuy}>Buy this Coupon</RedeemButton> :
+                            <Button variant='contained' disabled>Bought</Button>
+                        }
+                    </div>
+                </div>
             </div>
-                    
 
-    </div>
-)
+
+        </div>
+    )
 }
